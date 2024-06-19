dc84eb6f322192e82d3f8fa0a8b6546b2be5be18
Gyomiel <rosadrdlm@gmail.com>
2024-06-19 16:43:33 +0100

    GENERAL: -Added the header (not styled yet). -Started the footer.

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,1 +1,15 @@
 import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+        <div>
+          <p>Copyright © 2024 | Made with ♥︎ by Mil Leches Team •ᴗ• | All rights deserved. </p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
