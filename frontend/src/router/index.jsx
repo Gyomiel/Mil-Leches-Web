48c83ba9a4a74f182421b32e0b38689bf56cff09
Gyomiel <rosadrdlm@gmail.com>
2024-06-19 16:14:06 +0100

    GENERAL: -Added main pages and routes. -Working on the header.

---
+++ frontend/src/router/index.jsx
@@ -0,0 +1,43 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../layout/Layout";
+import Home from "../pages/Home/Home";
+import AboutUs from "../pages/AboutUs/AboutUs";
+import Services from "../pages/Services/Services";
+import Help from "../pages/Help/Help";
+import SignUp from "../pages/SignUp/SignUp";
+import Login from "../pages/Login/Login";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/aboutus",
+        element: <AboutUs />,
+      },
+      {
+        path: "/services",
+        element: <Services />
+      },
+      {
+        path: "/help",
+        element: <Help />
+      },
+      {
+        path: '/signup',
+        element: <SignUp />
+      },
+      {
+        path: '/login',
+        element: <Login />
+      }
+    ],
+  },
+]);
+
+export default router;
