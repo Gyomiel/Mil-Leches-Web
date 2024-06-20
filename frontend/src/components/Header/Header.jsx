ce9cde1afc18343332b677d4ce8cc20daece8042
Gyomiel <rosadrdlm@gmail.com>
2024-06-20 12:51:31 +0100

    Header and footer done (they work).

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,13 +1,28 @@
 import "./Header.css";
 import logo from "../../assets/logo.png";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const Header = () => {
+  const { pathname } = useLocation();
+  const [isAuth, setIsAuth] = useState(false);
+  useEffect(() => {
+    showButtons();
+  }, [pathname]);
+  const showButtons = () => {
+    if (pathname === "/signup" || pathname === "/login") {
+      return setIsAuth(true);
+    } else {
+      setIsAuth(false);
+    }
+  };
   return (
     <div id="container">
       <header>
         <section id="headerLogo">
-          <img src={logo} />
+          <Link to="/">
+            <img src={logo} />
+          </Link>
         </section>
         <div className="divider"></div>
         <section id="links">
@@ -20,12 +35,12 @@ const Header = () => {
           <div>
             <Link to="/help">Help</Link>
           </div>
-          <div>
+          <div className={isAuth ? "hideButton" : "showButton"}>
             <Link to="/signup">
               <button className="subtn">Sign Up</button>
             </Link>
           </div>
-          <div>
+          <div className={isAuth ? "hideButton" : "showButton"}>
             <Link to="/login">
               <button className="libtn">Log in</button>
             </Link>
