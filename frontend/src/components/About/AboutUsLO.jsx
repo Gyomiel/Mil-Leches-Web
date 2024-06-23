dfbb31acb63a2bee5298c32260d7a787f0ec81b2
Gyomiel <rosadrdlm@gmail.com>
2024-06-23 18:58:40 +0100

    Help and About pages almost styled.

--- frontend/src/components/About/AboutUsLO.jsx
+++ frontend/src/components/About/AboutUsLO.jsx
@@ -1,9 +1,39 @@
 import './AboutUsLO.css';
+import aboutUsIcon from '../../assets/aboutUsIcon.png'
 
 const AboutUsLO = () => {
   return (
     <div>
-      This is AboutUsLO
+      <section id='services-box'>
+        <h1 className='servHeader'>About Mil Leches and our purpose</h1>
+        <div>
+          <p className='servDesc'>In our current fast-paced world, life can be quite demanding for pet owners. Between work obligations, social engagements, and personal time, giving our pets undivided attention is not always feasible. </p>
+          <p className='servDesc'><span>Whether you are working late, traveling, or just need a respite, Mil Leches is your solution!</span></p>
+          <p className='servDesc'>We offer services such as home-sitting, boarding, dog walking, and grooming, connecting you with passionate pet lovers who treat your furry companions as their own in a paradise of cuddles and belly rubs.</p>
+        </div>
+        <div>
+          <img className='aboutUsIcon' src={aboutUsIcon} alt="" />
+        </div>
+      </section>
+      <section id='members'>
+        <h3 className='hSHeader'>Mil Leches Team</h3>
+        <div>
+          <img src="" alt="" />
+          <h5>Marta</h5>
+          <p className='servDesc'>Head back-end designer and animal lover.</p>
+        </div>
+        <div>
+        <img src="" alt="" />
+          <h5>Diana</h5>
+          <p className='servDesc'>Head front-end designer and pet cuddles enthusiast.</p>
+        </div>
+        <div>
+        <img src="" alt="" />
+          <h5>Borja</h5>
+          <p className='servDesc'>Head of IT and dreaming of drawing full time.</p>
+        </div>
+        <button className='aboutButton'>Get started!</button>
+      </section>
     </div>
   );
 };
--- frontend/src/components/Help/HelpLayout.jsx
+++ frontend/src/components/Help/HelpLayout.jsx
@@ -1,9 +1,32 @@
 import './HelpLayout.css';
+import searchHelp from '../../assets/searchHelp.png';
+import chatHelp from '../../assets/chatHelp.png';
+import hatHelp from '../../assets/hatHelp.png';
 
 const HelpLayout = () => {
   return (
     <div>
-      This is HelpLayout
+      <section id='services-box'>
+        <h1 className='servHeader'>FIND YOUR PERFECT PET SITTER ON <span>MIL LECHES</span></h1>
+          <div className='servicesCards'>
+            <div className='findSitter'>
+              <img src={searchHelp} />
+              <h3 className='hSHeader'>Find your pet sitter</h3>
+              <p className='servDesc'>Use the search bar in your profile to locate all the available pet sitters on your preferred island.</p>
+            </div>
+            <div className='getInTouch'>
+              <img src={chatHelp} />
+              <h3 className='hSHeader'>Get in touch</h3>
+              <p className='servDesc'>Start chatting with the pet sitter to finalize all the important details, including the starting time, rate, and any special needs your pet may have.</p>
+            </div>
+            <div className='enjoyYourTime'>
+              <img src={hatHelp} />
+              <h3 className='hSHeader'>Enjoy your time</h3>
+              <p className='servDesc'>The pet sitter focuses their attention on caring for your pets, ensuring they receive ample love, companionship, and personalized care throughout their stay.</p>
+            </div>
+          </div>
+          <button className='helpButton'>Contact us for assistance!</button>
+      </section>
     </div>
   );
 };
